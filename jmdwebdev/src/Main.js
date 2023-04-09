b32e597018e1412fec8f330a679da945a129f643
unknown <j.dabadie81@gmail.com>
2023-04-09 10:02:16 +0200

    beginning main page

---
+++ jmdwebdev/src/Main.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Main.css"; 
+
+function MainPage() {
+    return (
+      <div >
+        <h1 className="animate__animated animate__zoomIn">This is JMD Web Development</h1>
+        <h2>Créations | Gestion</h2>
+        
+        <p className="animate__animated animate__fadeIn">Bienvenue sur jmdwebdev.com !</p>
+        
+        <p>Je suis Julien Dabadie, développeur Web depuis plus d'un an. J'ai créé plusieurs sites pour des clients et pour mes projets personnels. Je suis titulaire du titre professionnel Développeur Web et Web Mobile reconnu d'État, équivalent à un niveau bac +2. J'ai acquis de bonnes connaissances dans plusieurs langages de programmation, notamment C#, PHP et JavaScript. Mon expérience m'a permis de maîtriser les compétences requises pour le développement Web, la conception de sites responsives, ainsi que pour l'utilisation de différentes technologies et frameworks. Je suis toujours en veille technologique pour fournir à mes clients des sites modernes et performants.</p>
+      </div>
+    );
+  }
+  
+  export default MainPage;
+  
