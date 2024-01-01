3d409124dbd54584f97ec543a5ee4799ecc66c68
unknown <j.dabadie81@gmail.com>
2024-01-01 19:20:32 +0100

    navbar completed

--- jmdwebdev/Frontend/src/Components/Navbar/Navbar.js
+++ jmdwebdev/Frontend/src/Components/Navbar/Navbar.js
@@ -1,39 +1,40 @@
-import React, { useContext} from 'react';
+import React, { useState, useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { DarkModeContext } from '../DarkMode/DarkModeContext';
-
 import './Navbar.css';
 
 function Navbar() {
+  const { darkMode, setDarkMode } = useContext(DarkModeContext);
+  const [click, setClick] = useState(false);
 
-const { darkMode, setDarkMode } = useContext(DarkModeContext);
-const language = localStorage.getItem('language') || 'en';
-const home = language==='fr' ? 'Page d\'accueil':'Home';
-const about = language==='fr' ? 'A propos':'About';
-const contact= language==='fr' ? 'Me contacter':'Contact Me';
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
 
+  const language = localStorage.getItem('language') || 'en';
+  const home = language === 'fr' ? 'Page d\'accueil' : 'Home';
+  const about = language === 'fr' ? 'A propos' : 'About';
+  const contact = language === 'fr' ? 'Me contacter' : 'Contact Me';
 
-
- 
   const toggleDarkMode = () => {
-        setDarkMode(!darkMode);
+    setDarkMode(!darkMode);
   };
 
   return (
     <nav className={`navbar ${darkMode ? 'dark' : 'light'}`}>
       <div className="navbar-container">
-        <Link to="/" className="navbar-logo">
-          JMD Web Dev
-        </Link>
-        <ul className="nav-menu">
+        
+        <div className="menu-icon" onClick={handleClick}>
+          <i className={click ? 'fa fa-times' : 'fa fa-bars'}></i>
+        </div>
+        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
           <li className="nav-item">
-            <Link to="/" className="nav-links">{home}</Link>
+            <Link to="/" className="nav-links" onClick={closeMobileMenu}>{home}</Link>
           </li>
           <li className="nav-item">
-            <Link to="/About" className="nav-links">{about}</Link>
+            <Link to="/About" className="nav-links" onClick={closeMobileMenu}>{about}</Link>
           </li>
-            <li className="nav-item">
-            <Link to="/Contact" className="nav-links">{contact}</Link>
+          <li className="nav-item">
+            <Link to="/Contact" className="nav-links" onClick={closeMobileMenu}>{contact}</Link>
           </li>
         </ul>
         <i className={`fa ${darkMode ? 'fa-sun-o' : 'fa-moon-o'}`} onClick={toggleDarkMode}></i>
@@ -41,7 +42,5 @@ const contact= language==='fr' ? 'Me contacter':'Contact Me';
     </nav>
   );
 }
-
-
 
 export default Navbar;
